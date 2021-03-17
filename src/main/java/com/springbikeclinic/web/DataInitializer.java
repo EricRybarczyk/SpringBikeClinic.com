4f8fdbeaa0659247ef72d437e429044484f89a69
EricRybarczyk <EricRybarczyk@gmail.com>
2021-03-16 20:19:10 -0400

    Load Realistic WorkType Values in DataInitializer

--- src/main/java/com/springbikeclinic/web/DataInitializer.java
+++ src/main/java/com/springbikeclinic/web/DataInitializer.java
@@ -1,8 +1,9 @@
 package com.springbikeclinic.web;
 
-import com.springbikeclinic.web.domain.WorkType;
 import com.springbikeclinic.web.domain.security.Authority;
 import com.springbikeclinic.web.domain.security.User;
+import com.springbikeclinic.web.dto.WorkTypeDto;
+import com.springbikeclinic.web.mappers.WorkTypeMapper;
 import com.springbikeclinic.web.repositories.WorkTypeRepository;
 import com.springbikeclinic.web.repositories.security.AuthorityRepository;
 import com.springbikeclinic.web.repositories.security.UserRepository;
@@ -17,13 +18,15 @@ import java.math.BigDecimal;
 public class DataInitializer implements CommandLineRunner {
 
     private final WorkTypeRepository workTypeRepository;
+    private final WorkTypeMapper workTypeMapper;
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
     private final PasswordEncoder passwordEncoder;
     private static final Logger log = LoggerFactory.getLogger(DataInitializer.class);
 
-    public DataInitializer(WorkTypeRepository workTypeRepository, UserRepository userRepository, AuthorityRepository authorityRepository, PasswordEncoder passwordEncoder) {
+    public DataInitializer(WorkTypeRepository workTypeRepository, WorkTypeMapper workTypeMapper, UserRepository userRepository, AuthorityRepository authorityRepository, PasswordEncoder passwordEncoder) {
         this.workTypeRepository = workTypeRepository;
+        this.workTypeMapper = workTypeMapper;
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
         this.passwordEncoder = passwordEncoder;
@@ -69,18 +72,63 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void loadWorkTypes() {
-        for (int i = 1; i < 7; i++) {
-            WorkType workType = new WorkType();
-            workType.setName("Work Type " + i);
-            workType.setDescription("This is a Service offered by Spring Bike Clinic, and it is a really good service and you should really get this excellent service for your bike today!");
-            workType.setSortPriority(i);
-            if (i % 2 == 1) {
-                workType.setPrice(BigDecimal.valueOf(79.95));
-            } else {
-                workType.setPrice(BigDecimal.valueOf(119.95));
-            }
-            workTypeRepository.save(workType);
-        }
+
+        WorkTypeDto workTypeTuneUp = WorkTypeDto.builder()
+                .name("Tune Up")
+                .description("Our best seller, the Tune Up will keep your wheels turning all season long. Includes derailleur adjustments, drive train cleaning, and basic brake tuning.")
+                .price(BigDecimal.valueOf(99.95))
+                .sortPriority(1)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeTuneUp));
+
+        WorkTypeDto workTypeDerailleur = WorkTypeDto.builder()
+                .name("Derailleur Adjustment")
+                .description("Front or rear derailleur tuning. We'll get your shift together!")
+                .price(BigDecimal.valueOf(19.95))
+                .sortPriority(2)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeDerailleur));
+
+        WorkTypeDto workTypeFlatTire = WorkTypeDto.builder()
+                .name("Flat Tire")
+                .description("Get back on your bike after we replace the tube in your flat tire. Includes replacement tube.")
+                .price(BigDecimal.valueOf(44.95))
+                .sortPriority(3)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeFlatTire));
+
+        WorkTypeDto workTypeTrueWheel = WorkTypeDto.builder()
+                .name("True a Wheel")
+                .description("Feeling a bit wobbly? Truing a wheel restores the straight path in front of your bike.")
+                .price(BigDecimal.valueOf(39.95))
+                .sortPriority(4)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeTrueWheel));
+
+        WorkTypeDto workTypeSuspension = WorkTypeDto.builder()
+                .name("Suspension Service")
+                .description("Keep the plush feeling as you hit the big drops by maintaining this critical component. Front fork or rear shock service available.")
+                .price(BigDecimal.valueOf(89.95))
+                .sortPriority(5)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeSuspension));
+
+        WorkTypeDto workTypeBrakeBleed = WorkTypeDto.builder()
+                .name("Brake Bleed")
+                .description("Get those hydraulics back in tip top shape with fresh fluid and a firm lever feel. Includes brake safety inspection.")
+                .price(BigDecimal.valueOf(49.95))
+                .sortPriority(6)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeBrakeBleed));
+
+        WorkTypeDto workTypeBrakeService = WorkTypeDto.builder()
+                .name("Break Service")
+                .description("Caliper alignment, sticky pistons, worn pads or cables... We'll make sure you can stop when you need to. Replacement parts not inclded.")
+                .price(BigDecimal.valueOf(39.95))
+                .sortPriority(7)
+                .build();
+        workTypeRepository.save(workTypeMapper.workTypeDtoToWorkType(workTypeBrakeService));
+
 
         log.debug("Work Types Loaded: " + workTypeRepository.count());
     }
