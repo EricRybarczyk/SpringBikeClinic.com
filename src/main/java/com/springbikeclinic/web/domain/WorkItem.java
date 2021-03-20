993a07a23f556bf8b963dcfa3db0b868a243b12e
EricRybarczyk <EricRybarczyk@gmail.com>
2021-03-20 10:33:42 -0400

    Add Missing Price Field to WorkItem

--- src/main/java/com/springbikeclinic/web/domain/WorkItem.java
+++ src/main/java/com/springbikeclinic/web/domain/WorkItem.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
@@ -31,5 +32,7 @@ public class WorkItem {
 
     private String description;
     private String mechanicNotes;
+
+    private BigDecimal price;
 
 }
